ea0ae171eea0888a231396a3ca3640d2a8db4b12
BrandenXia <xxtbranden@outlook.com>
2025-12-24 18:22:48 +0800

    structured entrypoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,31 @@
 #include <fstream>
 #include <print>
+#include <string_view>
 
 #include "msg.h"
 
-int main() {
-  auto f = std::ofstream("data/msg.txt");
+void export_clean(std::string_view filename) {
+  auto f = std::ofstream(filename.data());
 
-  for (const auto &[time, content] : buddha::get_messages())
+  auto cnt = 0;
+  for (const auto &[time, content] : buddha::get_messages()) {
     f << content << "\n\n";
+    cnt++;
+  }
   f.close();
 
-  std::print("Done!\n");
+  std::println("Finished exporting {} messages to {}", cnt, filename);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::print("Usage: {} <mode>\n", argv[0]);
+    return 1;
+  }
+
+  auto mode = std::string_view{argv[1]};
+  if (mode == "export")
+    export_clean("data/msg.txt");
+  else
+    std::println("Unknown mode: {}", mode);
 }
