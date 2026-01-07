f91e1442f1f950026c246d07f9900f96478ef989
BrandenXia <xxtbranden@outlook.com>
2026-01-07 09:08:13 -0800

    refactor formats

---
+++ include/formats.h
@@ -0,0 +1,18 @@
+#ifndef BUDDHA_REPLY_FORMATS_H
+#define BUDDHA_REPLY_FORMATS_H
+
+#include <filesystem>
+
+namespace buddha::exports {
+
+auto text(std::filesystem::path filename) -> void;
+
+auto sft_jsonl(std::filesystem::path filename) -> void;
+
+auto dpo_jsonl(std::filesystem::path filename) -> void;
+
+auto bpe(std::filesystem::path filename) -> void;
+
+} // namespace buddha::exports
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,135 +1,15 @@
-#include <cstdio>
 #include <filesystem>
-#include <fstream>
 #include <print>
-#include <ranges>
 #include <string_view>
 
-#include "rapidjson/document.h"
-#include "rapidjson/filewritestream.h"
-#include "rapidjson/prettywriter.h"
-
 #include "bpe.h"
-#include "msg.h"
+#include "formats.h"
 
-#define DB_PATH "data/msg.sqlite"
-
-auto export_text(std::filesystem::path filename) {
-  auto f = std::ofstream(filename);
-
-  auto cnt = 0;
-  for (const auto &[time, content] : buddha::get_messages(DB_PATH)) {
-    f << reinterpret_cast<const char *>(content.c_str()) << "\n\n";
-    cnt++;
-  }
-  f.close();
-
-  std::println("Finished exporting {} messages to {}", cnt, filename.c_str());
-}
-
-auto export_jsonl(std::filesystem::path filename) {
-  auto fp = std::fopen(filename.c_str(), "wb");
-  if (!fp) {
-    std::println(stderr, "Failed to open file: {}", filename.string());
-    return;
-  }
-
-  auto buffer = std::vector<char>(65536);
-  auto os = rapidjson::FileWriteStream(fp, buffer.data(), buffer.size());
-  auto writer = rapidjson::Writer<rapidjson::FileWriteStream>(os);
-
-  auto current_content = std::u8string{};
-  auto prev_author = std::string_view{};
-
-  auto is_user_role = true;
-  auto first_message = true;
-
-  for (const auto &[author, content] : buddha::get_messages(DB_PATH)) {
-    if (!first_message && author != prev_author) {
-      if (is_user_role) {
-        writer.StartObject();
-        writer.Key("messages");
-        writer.StartArray();
-      }
-
-      writer.StartObject();
-      writer.Key("role");
-      if (is_user_role)
-        writer.String("user", 4);
-      else
-        writer.String("assistant", 9);
-
-      writer.Key("content");
-      writer.String(reinterpret_cast<const char *>(current_content.c_str()),
-                    static_cast<rapidjson::SizeType>(current_content.size()));
-      writer.EndObject();
-
-      if (!is_user_role) {
-        writer.EndArray();
-        writer.EndObject();
-        os.Put('\n');
-
-        is_user_role = true;
-      } else
-        is_user_role = false;
-
-      current_content.clear();
-    }
-
-    if (!current_content.empty()) {
-      if (current_content.ends_with(content)) continue;
-
-      current_content += u8"\n";
-    }
-    current_content += content;
-
-    prev_author = author;
-    first_message = false;
-  }
-
-  if (!current_content.empty()) {
-    if (is_user_role) {
-      writer.StartObject();
-      writer.Key("messages");
-      writer.StartArray();
-    }
-
-    writer.StartObject();
-    writer.Key("role");
-    if (is_user_role)
-      writer.String("user", 4);
-    else
-      writer.String("assistant", 9);
-
-    writer.Key("content");
-    writer.String(reinterpret_cast<const char *>(current_content.c_str()),
-                  static_cast<rapidjson::SizeType>(current_content.size()));
-    writer.EndObject();
-
-    writer.EndArray();
-    writer.EndObject();
-    os.Put('\n');
-  }
-
-  os.Flush();
-  std::fclose(fp);
-}
-
-auto build_bpe() {
-  // clang-format off
-  const auto msgs = buddha::get_messages(DB_PATH)
-    | std::views::transform([](const auto &pair) { return pair.second; })
-    | std::views::join
-    | std::ranges::to<std::u8string>();
-  // clang-format on
-  auto t = buddha::bpe::build(msgs);
-  buddha::bpe::print_table(t);
-  buddha::bpe::export_table("data/msg.bpe", t);
-}
+using namespace buddha;
 
 auto import_bpe() {
-  auto t = buddha::bpe::import_table("data/msg.bpe");
-  buddha::bpe::print_table(t);
+  auto t = bpe::import_table("data/msg.bpe");
+  bpe::print_table(t);
 }
 
 auto main(int argc, char *argv[]) -> int {
@@ -142,14 +22,14 @@ auto main(int argc, char *argv[]) -> int {
   if (mode == "export") {
     auto type = argc == 3 ? std::string_view{argv[2]} : "text";
     if (type == "text")
-      export_text("data/msg.txt");
-    else if (type == "jsonl")
-      export_jsonl("data/msg.jsonl");
+      exports::text("data/msg.txt");
+    else if (type == "sft-jsonl")
+      exports::sft_jsonl("data/msg.jsonl");
+    else if (type == "bpe")
+      exports::bpe("data/msg.bpe");
     else
       std::println("Unknown export type: {}", type);
-  } else if (mode == "bpe")
-    build_bpe();
-  else if (mode == "import")
+  } else if (mode == "import")
     import_bpe();
   else
     std::println("Unknown mode: {}", mode);
